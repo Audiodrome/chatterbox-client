52f366920d933fea49ed11a28bc730af27b83ddc
Hack Reactor Student <student@hackreactor.com>
2017-08-25 16:14:00 -0700

    Able to receive messages and send messages using AJAX get and post request. Working on styling css for each individual messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,102 @@
 // YOUR CODE HERE:
+
+$(document).ready(function() {
+  app.init();
+  //console.log(window.location.search);
+  data.username = window.location.search;
+  //console.log(data);
+  $('#submitmsg').click(function(event) {
+    event.preventDefault();
+    data.message = $('#msgbox').val();
+    $('#msgbox').val('');
+    app.renderMessage(data.message);
+    app.send(data);
+  });
+
+  $('#clearmsgbox').click(function(event) {
+    event.preventDefault();
+    app.clearMessages();
+  });
+    
+  
+  // var html = 
+  // $('#chats').append('<span>OMG IT\'s 1998!</span><br>');
+  // $('#chats').append('<span>FUUUUUUUUU!</span><br>');
+  // app.fetch();
+});
+
+let data = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+// let App = function() {
+//   this.server = 'http://parse.la.hackreactor.com/chatterbox/classes/messages';
+// };
+
+let app = {};
+// let app = new App
+
+app.server = 'http://parse.la.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+  return 0;
+};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    success: function (data) {
+      console.log(data, 'chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  var node = $('#chats').children();
+  for (var i = 0; i < node.length; i++) {
+    node.remove(node.children[i]);
+  }
+
+  // var node = $('.chat').children();
+  // for (var i = 0; i < node.length; i++) {
+  //   node.remove(node.children[i]);
+  // }
+};
+
+app.renderMessage = function(message) {
+
+  // console.log($('#chats').children().length);
+  $('#chats').append('<div class="chat">' + message + '</div>');
+  // console.log($('#chats').children().length);
+
+  // $('.chat').append('<span>' + message + '</span><br>');
+
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append('<option>' + roomName + '</option>');
+}
